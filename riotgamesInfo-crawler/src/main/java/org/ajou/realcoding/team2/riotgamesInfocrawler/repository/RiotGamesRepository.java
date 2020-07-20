8453b1f4f01a9aafb07ec2ed751e15e6524dd3f8
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-20 20:30:52 +0900

    step 2 is updated

--- riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/repository/RiotGamesRepository.java
+++ riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/repository/RiotGamesRepository.java
@@ -1,8 +1,38 @@
 package org.ajou.realcoding.team2.riotgamesInfocrawler.repository;
 
+import org.ajou.realcoding.team2.riotgamesInfocrawler.api.RiotGamesOpenApiClient;
 import org.ajou.realcoding.team2.riotgamesInfocrawler.domain.FinalGameInformation;
+import org.ajou.realcoding.team2.riotgamesInfocrawler.domain.SummonerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RiotGamesRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @Autowired
+    private RiotGamesOpenApiClient riotGamesOpenApiClient;
+
+    public void saveSummonerInfo(SummonerDto summoner){
+        SummonerDto savedSummoner = mongoTemplate.save(summoner);
+
+    }
+    public SummonerDto getSummonerInform(String summonerName) {
+        Query query = Query.query(Criteria.where("_id").is(summonerName));
+        SummonerDto summonerA = mongoTemplate.findOne(query, SummonerDto.class);
+
+        if(summonerA == null){
+            SummonerDto summoner = riotGamesOpenApiClient.getSummonerInfo(summonerName);
+            saveSummonerInfo(summoner);
+            return summoner;
+        }else{
+            return summonerA;
+        }
+    }
+
     public FinalGameInformation findFinalGameInformation(String summonerName) {
         return null;
     }
