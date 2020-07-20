703c9b6c3ef2a37b15b23b5f80550ed5a8f21b37
Setoa <diqzk528@naver.com>
2020-07-20 21:57:26 +0900

    step 3 is completed, league info is updated (#2)

--- riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/api/RiotGamesOpenApiClient.java
+++ riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/api/RiotGamesOpenApiClient.java
@@ -1,5 +1,6 @@
 package org.ajou.realcoding.team2.riotgamesInfocrawler.api;
 
+import org.ajou.realcoding.team2.riotgamesInfocrawler.domain.LeagueEntryDto;
 import org.ajou.realcoding.team2.riotgamesInfocrawler.domain.SummonerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,20 @@ public class RiotGamesOpenApiClient {
     private RestTemplate restTemplate;
 
     private static final String SUMMONERINFO_REQUEST = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key=RGAPI-2c484922-2e11-4acc-a79d-a1cb78210959";
+    private static final String LEAGUEINFO_REQUEST = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}?api_key=RGAPI-042ddc06-283d-4f72-9891-7eb5f920cc58";
+
 
     public SummonerDto getSummonerInfo(String summonerName){
         SummonerDto summoner = restTemplate.getForObject(SUMMONERINFO_REQUEST, SummonerDto.class, summonerName);
         return summoner;
+    }
+
+    public LeagueEntryDto getLeagueInfo(String summonerId){
+        LeagueEntryDto[] leagues = restTemplate.getForObject(LEAGUEINFO_REQUEST, LeagueEntryDto[].class, summonerId);
+        for(LeagueEntryDto league : leagues) {
+            if (league.getQueueType().equals("RANKED_SOLO_5x5")) return league;
+        }
+        return null;
     }
 
 }
--- riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/domain/LeagueEntryDto.java
+++ riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/domain/LeagueEntryDto.java
@@ -1,4 +1,14 @@
 package org.ajou.realcoding.team2.riotgamesInfocrawler.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
 public class LeagueEntryDto {
+    private String tier;
+    private String rank;
+    private String queueType;
+    @Id
+    private String summonerId;
+    private String summonerName;
 }
