ff0e57f40f8ecfa859fa760c3a8117462fb06e4b
nyw <skdud@ajou.ac.kr>
2020-07-20 22:15:53 +0900

    Update matchlist info

--- riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/domain/MatchDto.java
+++ riotgamesInfo-crawler/src/main/java/org/ajou/realcoding/team2/riotgamesInfocrawler/domain/MatchDto.java
@@ -1,4 +1,17 @@
 package org.ajou.realcoding.team2.riotgamesInfocrawler.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
 public class MatchDto {
+    @Id
+    private String accountId;
+    private List<Matches> matches;
+    @Data
+    public static class Matches{
+        private String gameId;
+    }
 }
